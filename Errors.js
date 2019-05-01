3eee82e61906dda978c0ee5f9f8af54601f69759
Gabriel Fonseca <gabrieldfnsc@gmail.com>
2019-04-30 22:52:56 -0300

    Correcao de bugs

--- Errors.js
+++ Errors.js
@@ -1,68 +1,64 @@
-class Errors
-{
-	/**
-	 * Create a new Errors instance.
-	 */
-	constructor()
-	{
-		this.errors = {};
-	}
+class Errors {
+    /**
+     * Create a new Errors instance.
+     */
+    constructor() {
+        this.errors = {};
+    }
 
-	/**
-	 * Determine if an errors exists dor the given field.
-	 *
-	 * @param (string) field
-	 */
-	has (field)
-	{
-		return this.errors.hasOwnProperty(field);
-	}
 
-	/**
-	 * Determine if we have any errors.
-	 */
-	any ()
-	{
-		return Object.keys(this.errors).length > 0;
-	}
+    /**
+     * Determine if an errors exists for the given field.
+     *
+     * @param {string} field
+     */
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
 
-	/**
-	 * Retrieve the error message for a field.
-	 *
-	 * @param (string) field
-	 */
-	get (field)
-	{
-		if (this.errors[field]) {
-			this.errors[field][0];
-		}
-	}
 
-	/**
-	 * Record the new errors.
-	 *
-	 * @param (object) errors
-	 */
-	record (errors)
-	{
-		this.errors = errors;
-	}
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
 
-	/**
-	 * Clear one or all error fields.
-	 *
-	 * @param {string|null} field
-	 */
-	clear(field)
-	{
-		// On the form it will be called like that => @keydown="errors.clear($event.target.name)"
-		if (field)
-		{
-			delete this.errors[field];
-			return;
-		}
-		this.errors = {};
-	}
+
+    /**
+     * Retrieve the error message for a field.
+     *
+     * @param {string} field
+     */
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+
+    /**
+     * Record the new errors.
+     *
+     * @param {object} errors
+     */
+    record(errors) {
+        this.errors = errors;
+    }
+
+
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
+
+            return;
+        }
+
+        this.errors = {};
+    }
 }
-
-export default Errors;
--- Form.js
+++ Form.js
@@ -1,134 +1,130 @@
 import Errors from './Errors';
 
-class Form
-{
-	/**
-	 * Create a new Form instance
-	 *
-	 * @param {object} data
-	 */
-	constructor (data)
-	{
-		this.data = data;
-		for (let field in data) {
-			this[field] = data[field];
-		}
+class Form {
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
 
-		this.errors = new Errors();
-	}
+        for (let field in data) {
+            this[field] = data[field];
+        }
 
-	/**
-	 * Fetch all relevant data for the form.
-	 */
-	data ()
-	{
-		let data = {};
-		for (let property in this.originalData) {
-			data[property] = this[property];
-		}
-		// let data = Object.assign({}, this);
-		// delete data.originalData;
-		// delete data.errors;
-		return data;
-	}
+        this.errors = new Errors();
+    }
 
-	/**
-	 * Reset the form fields.
-	 */
-	reset ()
-	{
-		for (let field in this.originalData) {
-			this[field] = null;
-		}
-		this.errors.clear();
-	}
 
-	/**
+    /**
+     * Fetch all relevant data for the form.
+     */
+    data() {
+        let data = {};
+
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+
+        return data;
+    }
+
+
+    /**
+     * Reset the form fields.
+     */
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+
+        this.errors.clear();
+    }
+
+
+    /**
      * Send a POST request to the given URL.
      * .
      * @param {string} url
      */
-	post (url)
-	{
-		return this.submit('post', url);
-	}
+    post(url) {
+        return this.submit('post', url);
+    }
 
-	/**
+
+    /**
      * Send a PUT request to the given URL.
      * .
      * @param {string} url
      */
-	put (url)
-	{
-		return this.submit('put', url);
-	}
+    put(url) {
+        return this.submit('put', url);
+    }
 
-	/**
+
+    /**
      * Send a PATCH request to the given URL.
      * .
      * @param {string} url
      */
-	patch (url)
-	{
-		return this.submit('patch', url);
-	}
+    patch(url) {
+        return this.submit('patch', url);
+    }
 
-	/**
-     * Send a PATCH request to the given URL.
+
+    /**
+     * Send a DELETE request to the given URL.
      * .
      * @param {string} url
      */
-	delete (url)
-	{
-		return this.submit('delete', url);
-	}
+    delete(url) {
+        return this.submit('delete', url);
+    }
 
 
+    /**
+     * Submit the form.
+     *
+     * @param {string} requestType
+     * @param {string} url
+     */
+    submit(requestType, url) {
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
 
-	/**
-	 * Submit the form
-	 *
-	 * @param {string} requestType
-	 * @param {string} url
-	 */
-	submit (requestType, url)
-	{
-		return new Promisse ((resolve, reject) => {
-			axios[requestType](url, this.data())
-				.then(response => {
-					this.onSuccess(response.data);
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data);
 
-					resolve(response.data)
-				})
-				.catch(error => {
-					this.onFail(error.response.data);
-					reject(error.response.data);
-				});
+                    reject(error.response.data);
+                });
+        });
+    }
 
-		});
-	}
 
-	/**
-	 * Handle a successful form submission.
-	 *
-	 * @param {object} response
-	 */
-	onSuccess (data)
-	{
-		alert(data.message); // temporary
+    /**
+     * Handle a successful form submission.
+     *
+     * @param {object} data
+     */
+    onSuccess(data) {
+        alert(data.message); // temporary
 
-		this.reset();
-	}
+        this.reset();
+    }
 
-	/**
-	 * Handle a faild form submission.
-	 *
-	 * @param {object} error
-	 */
-	onFail (errors)
-	{
-		this.errors.record(errors);
-	}
+
+    /**
+     * Handle a failed form submission.
+     *
+     * @param {object} errors
+     */
+    onFail(errors) {
+        this.errors.record(errors);
+    }
 }
-
 export default Form;
